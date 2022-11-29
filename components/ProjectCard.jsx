8649e45f8fcf969eac0a9acea3ea976b07bbd4e7
Mehedi Bhuiyan <mehedibuiyan734@gmail.com>
2022-11-29 23:09:22 +0600

    projects

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import styles from "../styles/components/ProjectCard.module.scss";
-// import Image from "next/image";
+import Image from "next/image";
+import dummyImg from "../images/project-1.jpg";
 import { Data } from "./data";
 
 const ProjectCard = () => {
   console.log(Data);
   return (
     <div className={styles.card_con}>
-      <div className={styles.img_con}>{/* <Image /> */}</div>
+      <div className={styles.img_con}>
+        <Image src={dummyImg} height="" width="" alt="" />
+      </div>
+      <div className={styles.card_info}>
+        <p className={styles.uses_lan}>HTML SASS JavaScript Next.js</p>
+        <h1 className={styles.title}>ChertNodes</h1>
+        <p className={styles.desc}></p>
+        <div className={styles.btn_con}>
+          <button className={styles.btn}>Live</button>
+        </div>
+      </div>
     </div>
   );
 };
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
+import styles from "../styles/components/Projects.module.scss";
 
 const Projects = () => {
   return (
-    <div>
-      Projects
+    <div className={styles.projects_con}>
+      <div className={styles.projects_header}>
+        <h1>
+          <span>#</span>projects
+        </h1>
+        <button className={styles.btn}>View all ⇢</button>
+      </div>
       <ProjectCard />
     </div>
   );
