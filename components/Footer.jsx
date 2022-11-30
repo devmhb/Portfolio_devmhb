509feb7b71ea21961f4b6b89b111606c3f275f58
Mehedi Bhuiyan <mehedibuiyan734@gmail.com>
2022-12-01 03:07:24 +0600

    without footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styles from "../styles/components/Footer.module.scss";
+import Image from "next/image";
+import logo from "../images/logo primary.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.footer_con}>
+      <div className={styles.footer_wrapper}>
+        <div className={styles.row_left}>
+          <div className={styles.logo_con}>
+            <Image src={logo} height="" width="" alt="logo" />
+            Mehedi
+            <div className={styles.desc}>
+              Web designer and front-end developer based in Ukraine
+            </div>
+          </div>
+        </div>
+        <div className={styles.row_right}>
+          <h3>Media</h3>
+          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
